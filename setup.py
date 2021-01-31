60f8190ad4c0fb7de79aeadaca3565a7d46e7572
Matias Covarrubias <matias.covarrubias@nyu.edu>
2021-01-31 14:22:32 -0800

    Initialize stup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="marketsAI",
+    version="0.0.1",
+    install_requires=["gym"],
+    author="Matias Covarrubias",
+    author_email="matias.covarrubias@nyu.edu",
+)
