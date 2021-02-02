7a38e1cd51b7d21886fb92964c3e8b1086f1031a
Agustín Covarrubias <agucova@uc.cl>
2021-02-02 15:32:47 -0300

    feat: switch to poetry

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="marketsAI",
-    version="0.0.1",
-    install_requires=["gym"],
-    author="Matias Covarrubias",
-    author_email="matias.covarrubias@nyu.edu",
-)
