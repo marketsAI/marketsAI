24a677c5a5fa8986fb41309449043fdb510da25e
Matias Covarrubias <matias.covarrubias@nyu.edu>
2021-08-01 19:53:14 -0400

    expers and econ algos

--- marketsai/Econ_algos/run_GDSGE.m
+++ marketsai/Econ_algos/run_GDSGE.m
@@ -1,26 +1,25 @@
 clear all
 
 % State which model
-model = "capital_planner_1hh";
-path = '/Users/matiascovarrubias/Documents/universidad/NYU/Research/Repositories/marketsAI/marketsai/Econ_algos/' + model;
+model = "capital_planner_1hh_2z";
+%path = '/Users/matiascovarrubias/Documents/universidad/NYU/Research/Repositories/marketsAI/marketsai/Econ_algos/' + model;
 script = 'iter_'+model;
 
 %add path
-addpath path;
+%addpath '/Users/matiascovarrubias/Documents/universidad/NYU/Research/Repositories/marketsAI/marketsai/Econ_algos/' + model;
 
 % run iterations
 ItrRslt = eval(script);
 % Policy grid (I want a matrix with K rows and N_shocks as columns
 K = ItrRslt.var_state.K; % 1*101
-s = ItrRslt.var_policy.s; % 2*101
+s = ItrRslt.var_policy.s; % N_shocks * 101
 
 grid = [K',s'];
+writematrix(grid,'cap_planner_1hh_econ.csv')
 %transpose both and a
 
-%to do:
-% two shocks and parameteres of simulations. 
-% take it to python and from there create a compute_action that takes as
-% extra input the policy grid that 
+%Simulate
+SimuRslt = simulate_capital_planner_1hh_2z(ItrRslt);
 
 
 
